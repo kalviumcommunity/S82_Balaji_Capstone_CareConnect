404fcef957ca6c14c0486e8fc2a0d564c415f978
balaji <balajier2006@gmail.com>
2025-04-28 11:52:40 +0530

    schema updated

--- backend/models/patient.js
+++ backend/models/patient.js
@@ -5,7 +5,7 @@ const patientschema = new mongoose.Schema({
         type:String,required:true
     },
     email:{
-        type:String,required:true
+        type:String,required:true,unique:true
     },
     password:{
         type:String,required:true
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,20 @@ app.use(express.json())
 require('dotenv').config();
 const PORT = process.env.PORT
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+patientSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log('connected to database'))
+.catch((err)=>console.log(err));
 app.get('/',(req,res)=>{
     res.status(200).send('Hello From Backend!');
 })
