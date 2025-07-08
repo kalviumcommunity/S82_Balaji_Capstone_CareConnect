7fa0bdf3344ed7b055137b8d4c6134fff32c0a75
balaji <balajier2006@gmail.com>
2025-07-08 13:36:29 +0530

    done

--- Server/app.js
+++ Server/app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 app.use(cors({
   origin: [
     'https://extraordinary-kitsune4-f05960.netlify.app',  // ✅ production
-    'http://localhost:5173'                               // ✅ dev frontend
+    'http://localhost:5173'     // ✅ dev frontend
   ],
   credentials: true
 }));
