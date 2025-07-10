f956a31eac8dac996978643d51dc4ec4991850f3
balaji <balajier2006@gmail.com>
2025-07-10 16:49:18 +0530

    google-auth-updated

--- Server/config/passport.js
+++ Server/config/passport.js
@@ -8,7 +8,7 @@ console.log("Google Secret:", process.env.GOOGLE_CLIENT_SECRET);
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: process.env.GOOGLE_REDIRECT_URI
   },
   async (accessToken, refreshToken, profile, done) => {
     const email = profile.emails[0].value;
