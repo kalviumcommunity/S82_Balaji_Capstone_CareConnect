237303f09c7743894684a52ca99cd253069ac7d3
balaji <balajier2006@gmail.com>
2025-07-10 16:12:30 +0530

    Google-auth

--- Server/config/passport.js
+++ Server/config/passport.js
@@ -1,40 +1,28 @@
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import User from '../src/models/user.model.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/patient'); // or user schema you're using
 
 passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL,
-}, async (accessToken, refreshToken, profile, done) => {
-  try {
-    if (!profile.emails || profile.emails.length === 0) {
-      return done(new Error('No email found in Google profile'));
-    }
-    // console.log("accesstoken:",accessToken)
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/api/auth/google/callback"
+  },
+  async (accessToken, refreshToken, profile, done) => {
     const email = profile.emails[0].value;
-    const name = profile.displayName;
-
     let user = await User.findOne({ email });
 
     if (!user) {
-      user = new User({
-        name,
-        email,
-        password: '', // Google login doesn't need a password
-        // role: ['user'],
-        isActivated: true,
-      });
+      user = new User({ name: profile.displayName, email, isActivated: true });
       await user.save();
     }
 
-    // ✅ Return both profile and DB user
-    return done(null, { profile, user });
-
-  } catch (err) {
-    return done(err, null);
+    done(null, { profile, user });
   }
-}));
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
