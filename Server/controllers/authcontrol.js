5fcd834b4414beb6fe460e8b892dfc413e7cf25b
balaji <balajier2006@gmail.com>
2025-07-11 13:58:42 +0530

    fix: skip validation on Google signup

--- Server/controllers/authcontrol.js
+++ Server/controllers/authcontrol.js
@@ -242,7 +242,7 @@ exports.googleAuthCallback = async (req, res) => {
       maxAge: 72 * 60 * 60 * 1000,
     });
 
-    res.redirect(`https://extraordinary-kitsune6-f05960.netlify.app/google-success?token=${token}`);
+    res.redirect(`https://extraordinary-kitsune6-f50960.netlify.app/google-success?token=${token}`);
   } catch (err) {
     console.error("🔥 Google Auth Error:", err);
     res.status(500).json({ message: "Google Auth failed", error: err.message });
