87143f562e15f3342c01f80b7a3006370c282609
balaji <balajier2006@gmail.com>
2025-07-12 12:42:58 +0530

    done

--- client/src/pages/googleAuth/googlesigninbutton.jsx
+++ client/src/pages/googleAuth/googlesigninbutton.jsx
@@ -4,6 +4,7 @@ import React from 'react'
     const handleGoogleSignIn = () => {
         window.location.href = "https://s82-balaji-capstone-careconnect-4.onrender.com/api/auth/google";
       };
+      console.log(window.location.href)
     return (
         <button onClick={handleGoogleSignIn} className="bg-red-500 text-white px-4 py-2 rounded">
         Continue with Google
