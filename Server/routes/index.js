680926a7d3c90f537135300d183b16521e3f1ccf
balaji <balajier2006@gmail.com>
2025-08-03 10:32:10 +0530

    done

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
-const aiRoute = require('./ai');
+
 const patientRoute = require('./patientroute');
 const doctorRoute = require('./doctorroute');
 const authRoute = require('./auth');
-const appointmentRoutes = require('../routes/appointmentroute');
+const appointmentRoutes = require('./appointmentroute');
 
 router.use('/patients', patientRoute);
 router.use('/doctors', doctorRoute);
 router.use('/auth', authRoute);
-router.use('/patientprofile', patientRoute); // means prefix is /api/patientprofile
+router.use('/patientprofile', patientRoute);
 router.use('/appointments', appointmentRoutes);
-router.use('/', aiRoute);
 
 module.exports = router;
